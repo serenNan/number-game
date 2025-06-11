a49dbd5a7451ecffe4b60d00a7a41c5b77e0dab9
Serendipity <976758426@qq.com>
2025-06-11 21:33:14 +0800

    成功编译，但功能待完善

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 echo 正在编译...
-g++ -o number-game.exe pullze_main.cpp pullze_base.cpp pullze_console.cpp pullze_tools.cpp cmd_console_tools.cpp 
+g++ -o 90-b2.exe -std=c++11 -D_HAS_STD_BYTE=0 -DNOMINMAX -DWIN32_LEAN_AND_MEAN -fexec-charset=GBK -finput-charset=GBK -Wno-narrowing -Wno-write-strings pullze_main.cpp pullze_base.cpp pullze_console.cpp pullze_tools.cpp cmd_console_tools.cpp 
 if %ERRORLEVEL% EQU 0 (
     echo 编译成功！
     echo 正在运行程序...
-    number-game.exe
+    90-b2.exe
 ) else (
     echo 编译失败！
 )
